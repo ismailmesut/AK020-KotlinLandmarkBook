85835b20c00f15d36f7eda8b4e09f00db61c7683
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-05 10:16:32 +0300

    updated: ui design

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinlandmarkbook/DetailsActivity.kt
@@ -0,0 +1,17 @@
+package com.ismailmesutmujde.kotlinlandmarkbook
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinlandmarkbook.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var bindingDetailsActivity: ActivityDetailsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingDetailsActivity = ActivityDetailsBinding.inflate(layoutInflater)
+        val view = bindingDetailsActivity.root
+        setContentView(view)
+
+    }
+}
--- app/src/main/java/com/ismailmesutmujde/kotlinlandmarkbook/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinlandmarkbook/MainActivity.kt
@@ -2,12 +2,17 @@ package com.ismailmesutmujde.kotlinlandmarkbook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinlandmarkbook.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMainActivity: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMainActivity.root
+        setContentView(view)
 
     }
 }
