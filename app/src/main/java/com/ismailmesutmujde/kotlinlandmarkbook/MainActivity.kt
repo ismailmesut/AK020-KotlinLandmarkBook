e783dd029e34b73d5ce393feb9feab04f1b203fd
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-05 10:33:03 +0300

    updated: listview

--- app/src/main/java/com/ismailmesutmujde/kotlinlandmarkbook/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinlandmarkbook/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ismailmesutmujde.kotlinlandmarkbook
 
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.ismailmesutmujde.kotlinlandmarkbook.databinding.ActivityMainBinding
 
@@ -28,5 +30,12 @@ class MainActivity : AppCompatActivity() {
         landmarkList.add(colosseum)
         landmarkList.add(eiffel)
         landmarkList.add(londonBridge)
+
+        // Adapter : Layout & Data
+
+        // Mapping
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, landmarkList.map { landmark -> landmark.name })
+        bindingMainActivity.listView.adapter = adapter
+
     }
 }
